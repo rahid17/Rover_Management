8a963b6d90c199067bde32144b0d3269f90ac150
rahid17 <you@example.com>
2023-11-20 03:09:23 +0600

    update

--- lib/app/modules/navView/controllers/nav_view_controller.dart
+++ lib/app/modules/navView/controllers/nav_view_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:new_project/app/modules/home/views/home_view.dart';
 import 'package:new_project/app/modules/message/views/message_view.dart';
 import 'package:new_project/app/modules/profile/views/profile_view.dart';
+import 'package:new_project/app/routes/app_pages.dart';
 
 class NavViewController extends GetxController {
   RxInt selectIndex =0.obs;
@@ -17,8 +18,12 @@ class NavViewController extends GetxController {
   }
 
 
-  logOut(){
-    onClose();
-  }
 
+ 
+  logOut(){
+
+    
+   Get.offAllNamed(Routes.LOGIN);
+    
+  }
 }
